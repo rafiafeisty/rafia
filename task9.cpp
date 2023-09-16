8759fc6da524b5bc1d77b50cb6828175f10d7f52
rafiafeisty <144408051+rafiafeisty@users.noreply.github.com>
2023-09-16 13:04:34 -0700

    Add files via upload

---
+++ task9.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main(){
+system("Color 07");
+cout<< "  - - - - - - - - - - - - - - - - - - - - - - - - -           " << endl;
+cout << endl;
+cout<< "         o     ^ ___ ^                                        " << endl;
+
+cout<< "            o  ( O O )\\_________                             " << endl;
+
+cout<< "                (___)\\           )\\/\\                      " << endl;
+cout << endl;
+cout<< "                     || - - - -W  |                           " << endl;
+
+cout<< "                     ||          ||                           "<< endl;
+}
